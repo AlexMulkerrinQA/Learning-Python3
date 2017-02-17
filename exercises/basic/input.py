46f247b83816383c8561fe5a05fb1606f65b8993
AlexMulkerrinQA <Alexander.Mulkerrin@qa.com>
2017-02-17 15:55:24 +0000

    Example Python code for Boids is much when converted to JavaScript!

--- exercises/basic/input.py
+++ exercises/basic/input.py
@@ -1,41 +1,42 @@
-import random, time
+import random
+
 
 class MathBotV6:
-	numberArray = []
-	
-	def solve(self, int1, int2, isMultiply):
-		if int1==0 or int2==0:
-				return max(int1,int2)
-		
-		if isMultiply:
-			return int1 * int2
-		else:
-			return int1 + int2
-	
-	def setnumberArray(self, length):
-		self.numberArray = []
-		for i in range(length):
-			self.numberArray.append(random.randint(0,10))
-		
-	def loopMultiply(self,multiplier):
-		index=0
-		for i in self.numberArray:
-			self.numberArray[index] = self.solve(i,multiplier,True)
-			index+=1
-			
-	def getInput(self, prompt):
-		number = -1
-		while number<0:
-			userIn = input(prompt)
-			try:
-				number = int(userIn)
-			except ValueError:
-				print("Error: "+userIn+" is not a valid number",)
-			else:
-				if number<0:
-					print("Error: number must be positive",userIn)
-		#print(str(number) + " is a valid number",)
-		return number
+    numberArray = []
+
+    def solve(self, int1, int2, isMultiply):
+        if int1 == 0 or int2 == 0:
+                return max(int1, int2)
+
+        if isMultiply:
+            return int1 * int2
+        else:
+            return int1 + int2
+
+    def setnumberArray(self, length):
+        self.numberArray = []
+        for i in range(length):
+            self.numberArray.append(random.randint(0, 10))
+
+    def loopMultiply(self, multiplier):
+        index = 0
+        for i in self.numberArray:
+            self.numberArray[index] = self.solve(i, multiplier, True)
+            index += 1
+
+    def getInput(self, prompt):
+        number = -1
+        while number < 0:
+            userIn = input(prompt)
+            try:
+                number = int(userIn)
+            except ValueError:
+                print("Error: "+userIn+" is not a valid number",)
+            else:
+                if number < 0:
+                    print("Error: number must be positive", userIn)
+        # print(str(number) + " is a valid number",)
+        return number
 
 mathBot = MathBotV6()
 numIn = mathBot.getInput("Please give array size: ")
@@ -44,5 +45,3 @@ print(mathBot.numberArray)
 multIn = mathBot.getInput("Please give multiplier: ")
 mathBot.loopMultiply(multIn)
 print(mathBot.numberArray)
-
-
---
+++ exercises/intermediate/blackjack.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 11:28:07 2017
+
+@author: Alex
+"""
+
+userIn = input("Deal?")
+print("You said: "+userIn)
